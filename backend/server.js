0711dfb3b8e6f4e2a903b1f580e5de34eb279d8b
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-09-24 21:28:24 +0530

    Start working on auction application | setup project | create server | database connection | add dotenv and gitignore | download depandences

---
+++ backend/server.js
@@ -0,0 +1,61 @@
+//! Import packages
+const express = require('express');
+const colors = require('colors');
+const morgan = require('morgan');
+const app = express();
+const cors = require("cors");
+const dotenv = require('dotenv');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const db = require('./config/db');
+const fileUploade = require('express-fileupload')
+const cloudinary  = require('cloudinary')
+
+// !setup cloudinary
+cloudinary.v2.config({
+  cloud_name:process.env.CLOUDINARY_CLIENT_NAME,
+  api_key:process.env.CLOUDINARY_CLIENT_API,
+  api_secret:process.env.CLOUDINARY_CLIENT_SECRET
+})
+
+
+//!Middlewares
+// Middleware to access data from clint in JSON formate
+app.use(express.json())
+
+// Middleware for cross origin error
+app.use(cors({
+  origin:[process.env.FRONTEND_URL],
+  methods:["POST","GET","DELETE","PUT"],
+  credentials:true
+}));
+
+//* help to access cookies
+app.use(cookieParser());
+//* return data in json formate
+app.use(express.json());  
+app.use(express.urlencoded({extended:true}));
+app.use(fileUploade({
+  useTempFiles:true,
+  tempFileDir:"/temp/",
+}))
+
+// Middleware which tells us API method , status code and time taken by API
+app.use(morgan('dev'));
+
+
+// route
+// URL => http://localhost:4000
+
+
+app.get('/',(req,res)=>{
+  return res.status(200).json("Welcon to auction server");
+}) 
+
+
+const PORT  = process.env.PORT || 4000;
+
+// Start the server and listen for connections on port 3000
+app.listen(PORT,()=>{
+  console.log(`Server running on PORT : ${PORT}`.bgCyan);
+})
